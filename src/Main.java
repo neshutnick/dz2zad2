2b8d52e69839bc43adfc24447c9c42fae3662b32
neshutnick <neshutnick89@gmail.com>
2025-03-26 18:33:01 +0300

    result update

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        int balance = 300; //начальный баланс
-        int payment = 1235; //сумма пополнения
+        int balance = 350; //начальный баланс
+        int payment = 1135; //сумма пополнения
         int porog = 1000; //бонусный порог
         int bonus; // переменная для бонуса
         int result; //переменная для итогового баланса
 
         if (payment >= porog) {
             bonus = payment / 100;
-            result = balance + payment + bonus;
+
         } else {
             bonus = 0;
-            result = balance + payment;
         }
+
+        result = balance + payment + bonus;
+
         System.out.println("Бонус:" + bonus);
         System.out.println(("Баланс:" + result));
     }
